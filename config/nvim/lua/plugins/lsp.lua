65e674cc8ab1028ed2d8b90b7db3e012b22edb4e
cusox <cusoxlee@gmail.com>
2026-05-15 19:12:25 +0800

    feat: enable `lsp` support

--- config/nvim/lua/plugins/lsp.lua
+++ config/nvim/lua/plugins/lsp.lua
@@ -5,9 +5,19 @@ local gh = function(x)
 end
 
 function M.setup()
-	vim.pack.add(
-        gh("neovim/nvim-lspconfig")
-    )
+	vim.pack.add(gh("neovim/nvim-lspconfig"))
+
+	vim.lsp.enable("bashls")
+	vim.lsp.enable("clangd")
+	vim.lsp.enable("copilot")
+	vim.lsp.enable("docker_compose_language_service")
+	vim.lsp.enable("docker_language_server")
+	vim.lsp.enable("gopls")
+	vim.lsp.enable("harper_ls")
+	vim.lsp.enable("jsonls")
+	vim.lsp.enable("lua_ls")
+	vim.lsp.enable("neocmake")
+	vim.lsp.enable("ty")
 end
 
 return M
