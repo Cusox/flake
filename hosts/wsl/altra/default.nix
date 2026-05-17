816ee0dc903c661c751fc43fbee16c0de7a413e8
Cusox <cusoxlee@gmail.com>
2026-05-17 20:51:11 +0800

    fix: use `networking.resolvconf` configure DNS

--- hosts/wsl/altra/default.nix
+++ hosts/wsl/altra/default.nix
@@ -21,7 +21,15 @@
     };
   };
 
-  networking.hostName = hostName;
+  networking = {
+    hostName = hostName;
+
+    resolvconf.enable = true;
+
+    nameservers = [
+      "192.168.31.3"
+    ];
+  };
 
   environment = {
     pathsToLink = [ "/share/zsh" ];
@@ -31,10 +39,6 @@
       curl
       wget
     ];
-
-    etc."resolv.conf".txt = ''
-      nameserver 192.168.31.3
-    '';
   };
 
   programs.zsh.enable = true;
