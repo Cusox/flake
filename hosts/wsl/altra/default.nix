654a60ec88dedae868ef7379ba37e20de91cd657
Cusox <cusoxlee@gmail.com>
2026-05-17 14:56:38 +0000

    feat: provide `opengl-driver` support for `WSL`

--- hosts/wsl/altra/default.nix
+++ hosts/wsl/altra/default.nix
@@ -15,11 +15,14 @@
     defaultUser = user.username;
 
     wslConf = {
-      network = {
-        generateResolvConf = false;
-      };
+      boot.systemd = true;
+      network.generateResolvConf = false;
     };
+
+    useWindowsDriver = true;
   };
+
+  hardware.graphics.enable = true;
 
   networking = {
     hostName = hostName;
@@ -39,7 +42,23 @@
       curl
       wget
     ];
+
+    sessionVariables = {
+      LD_LIBRARY_PATH = [
+        "/run/opengl-driver/lib"
+        "${pkgs.openssl.out}/lib"
+      ];
+
+      LIBGL_DRIVERS_PATH = "${pkgs.mesa}/lib/dri";
+      GBM_BACKENDS_PATH = "${pkgs.mesa}/lib/gbm";
+
+      GALLIUM_DRIVER = "d3d12";
+      MESA_LOADER_DRIVER_OVERRIDE = "d3d12";
+      MESA_D3D12_DEFAULT_ADAPTER_NAME = "NVIDIA";
+    };
   };
+
+  services.dbus.enable = true;
 
   programs.zsh.enable = true;
 
