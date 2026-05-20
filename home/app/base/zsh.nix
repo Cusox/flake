355af563a61d615dddf00b287857e09e9cd6371f
Cusox <cusoxlee@gmail.com>
2026-05-20 13:15:55 +0800

    feat: modify `zsh` configuration

--- home/app/base/zsh.nix
+++ home/app/base/zsh.nix
@@ -7,7 +7,7 @@
     procs
 
     # Zsh Plugins
-    zsh-vi-mode
+    zsh-fzf-tab
     zsh-fast-syntax-highlighting
   ];
 
@@ -34,18 +34,22 @@
       "ps" = "procs";
     };
 
+    completionInit = "autoload -U compinit && compinit -i";
+
     autosuggestion = {
       enable = true;
     };
 
     sessionVariables = {
       LESSCHARSET = "utf-8";
+      SHELL = "${pkgs.zsh}/bin/zsh";
+      NODE_USE_ENV_PROXY = 1;
     };
 
     initContent = lib.mkMerge [
-      ''
-        source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
-      ''
+      (lib.mkOrder 600 ''
+        source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
+      '')
       (lib.mkAfter ''
         source ${pkgs.zsh-fast-syntax-highlighting}/share/zsh/plugins/fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
       '')
@@ -53,6 +57,10 @@
         fastfetch
       '')
     ];
+
+    envExtra = ''
+      [[ -r ~/.config/zsh/local.zshenv ]] && source ~/.config/zsh/local.zshenv
+    '';
 
     history = {
       append = true;
