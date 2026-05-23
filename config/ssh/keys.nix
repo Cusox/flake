9bbf7faae235a05d3794e80fd37ef18f97dce327
Cusox <cusoxlee@gmail.com>
2026-05-24 00:55:22 +0800

    feat: update `penguin` host ssh key

--- config/ssh/keys.nix
+++ config/ssh/keys.nix
@@ -1,4 +1,4 @@
 {
-  penguin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLYT6BbChqLnrAic9WR8Wgjqcbdrd8/L3r6RYNcZe+b penguin";
+  penguin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIA78F/h9zyK5d3r1SYTa5IzACEWu+q07fKZOFBWKpxDv penguin";
   altra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICde+QNrAA4ZmE4jp0TLcYBqo+EVCxVWHqVXLjuoSL95 altra";
 }
