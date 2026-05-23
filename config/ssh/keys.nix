b2445ab97da1ef8d11272af2e42c19f25b1983b4
Cusox <cusoxlee@gmail.com>
2026-05-23 16:25:35 +0000

    feat: add `altra` ssh pubkey

--- config/ssh/keys.nix
+++ config/ssh/keys.nix
@@ -1,3 +1,4 @@
 {
   penguin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMLYT6BbChqLnrAic9WR8Wgjqcbdrd8/L3r6RYNcZe+b penguin";
+  altra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICde+QNrAA4ZmE4jp0TLcYBqo+EVCxVWHqVXLjuoSL95 altra";
 }
