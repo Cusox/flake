9d1badee7612e83b68e33cfe150016b8e0042731
Cusox <cusoxlee@gmail.com>
2026-07-01 22:03:05 +0800

    feat: add nix proxy

--- flake.nix
+++ flake.nix
@@ -1,18 +1,6 @@
 {
   description = "Cusox's NixOS Flake";
 
-  # nixConfig = {
-  #   substituters = [
-  #     "https://mirrors.ustc.edu.cn/nix-channels/store/"
-  #     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store/"
-  #     "https://mirror.sjtu.edu.cn/nix-channels/store"
-  #     "https://nix-community.cachix.org/"
-  #   ];
-  #   trusted-public-keys = [
-  #     "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-  #   ];
-  # };
-
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
